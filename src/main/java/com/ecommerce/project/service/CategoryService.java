bf368f945fc9bce40854694e856c52299f4d8334
Tejveer <tejveer712@gmail.com>
2025-06-22 20:50:35 +0530

    building category module

---
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -0,0 +1,17 @@
+package com.ecommerce.project.service;
+import com.ecommerce.project.model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+//This is an interface not a class because we will go with loose coupling and modularity in my code base
+//Having seperate interface for service and a seperate implementation of that service is benifical in the long run
+
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+    void createCategory(Category category);
+
+    String deleteCategory(Long categoryId);
+    Category updateCategory(Category category, Long categoryId);
+}
