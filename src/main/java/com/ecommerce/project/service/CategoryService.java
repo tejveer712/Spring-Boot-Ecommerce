e546f5ff243bd447ed9fac09d7f932563a0e3dd1
Tejveer <tejveer712@gmail.com>
2025-07-02 07:42:22 +0530

    Completion of pagination implementation in this commit we added a standard for API response and added sorting in the API

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 
 public interface CategoryService {
-    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize);
+    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
     CategoryDTO createCategory(CategoryDTO categoryDTO);
     CategoryDTO deleteCategory(Long categoryId);
     CategoryDTO updateCategory(CategoryDTO categoryDTO, Long categoryId);
