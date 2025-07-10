b9d0dda5bbde5c43f75e73e4b0f6754ec853734c
Tejveer <tejveer712@gmail.com>
2025-07-10 07:29:52 +0530

    Adding product API

--- src/main/java/com/ecommerce/project/model/Product.java
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -15,10 +15,12 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long productId;
     private String productName;
+    private String image;
     private String description;
     private Integer quantity;
-    private Double price;
-    private Double specialPrice;
+    private double price;
+    private double discount;
+    private double specialPrice;
 
     @ManyToOne
     @JoinColumn(name = "category_id")
